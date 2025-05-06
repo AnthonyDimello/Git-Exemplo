8c24fe9743482d6e6c6175c8bf913fda5094a727
Anthony Nogueira <melo_anm@hotmail.com>
2025-05-06 12:42:16 -0300

    correção hello

--- src/gitexemplo/GitExemplo.java
+++ src/gitexemplo/GitExemplo.java
@@ -2,7 +2,7 @@
 package gitexemplo;
 public class GitExemplo {
     public static void main(String[] args) {
-        System.out.println("hello,world");
+        System.out.println("hello");
     }
     
 }
